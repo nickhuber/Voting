481e0efb1167b64c056eb3e88561dda9591b2e2d
Tom Nightingale <tom@tnightingale.com>
2010-10-02 00:20:21 -0700

    Codebase created with:   $: rails new pollster
    Added files to .gitignore according to advice here:
    http://inkfist.net/wiki/index.php/Version_Control_with_Git_and_Ruby_on_Rails_for_Debian

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pollster::Application.config.secret_token = 'd4187f0b056cddaef1f1a1c51df7a57d808b4a885b4b248725b228bade996edc66afbe5f1ddec91ddc6855d497a5e377377cf48a86faadf3d7f7d1269571459c'
