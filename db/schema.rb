d23e3728af3b5f37da84638f7ad8c9a7796e72e8
Tom Nightingale <tom@tnightingale.com>
2010-11-06 21:58:39 -0700

    using cancan for authorization, handles controller instance variable generation. Create a roles string field in the user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101107010302) do
+ActiveRecord::Schema.define(:version => 20101107045330) do
 
   create_table "active_polls", :force => true do |t|
     t.integer "poll_id"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(:version => 20101107010302) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
