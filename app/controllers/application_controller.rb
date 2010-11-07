88290dce82039d52d0f32bf5670ca16679c42d2e
Tom Nightingale <tom@tnightingale.com>
2010-11-07 00:21:49 -0700

    added auto redirect to login page if user is not logged in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   has_mobile_fu
   
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = "Access denied."
+    redirect_to :new_user_session
+  end
+  
   private
   def override_format
     request.format = :html
