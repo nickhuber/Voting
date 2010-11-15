d02ed877545f7eed2e71cf68d863f89b7dab2c38
Nick Huber <Nicholashuber@gmail.com>
2010-11-15 12:47:41 -0800

    my changes

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,14 @@ class Question < ActiveRecord::Base
   has_many :pollquestions
   has_many :polls, :through => :pollquestions
   has_many :answers
-  
   has_many :active_polls
+  
+  accepts_nested_attributes_for :answers
+  validate :length => {:min => 5}
+  
+  def answer_attributes=(answer_attributes)
+    answer_attributes.each do |attributes|
+      answers.build(attributes)
+    end
+  end
 end
