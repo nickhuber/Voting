5a5a74e60ed551030b391095690fc9f832e5554b
Tom Nightingale <tom@tnightingale.com>
2010-11-18 22:56:19 -0800

    session logic now correctly tracking users for the duration of one poll

--- app/models/active_poll.rb
+++ app/models/active_poll.rb
@@ -1,5 +1,5 @@
 class ActivePoll < ActiveRecord::Base
   belongs_to :poll
   belongs_to :question
-  has_one    :report
+  belongs_to :report
 end
--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,9 +1,27 @@
 class UserSession
+  
   def initialize(session)
     @session = session
   end
   
-  def add_active_poll(active_poll)
+  def id()
+    @session[:session_id]
+  end
+  
+  def active_poll()
+    @session[:active_poll_id]
+  end
+  
+  def active_poll=(active_poll)
     @session[:active_poll_id] = active_poll.id
+    participant = active_poll.report.participants.build
+    participant.user_session = id
+    if !participant.save
+      # Error check here
+    end
+  end
+  
+  def unbind()
+    @session[:active_poll_id] = nil;
   end
 end
