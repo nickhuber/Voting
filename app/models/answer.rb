ca277ed7ae665c4435088556456e5e23740ecdb1
Nick Huber <Nicholashuber@gmail.com>
2010-11-30 12:17:45 -0800

    removed correct answer checkbox until it works properly

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,9 +1,9 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
   
-  validates :body, :presence => true
+  validates_presence_of :body, :message => "Answer cannot be blank."
   
-  attr_accessor :should_destroy
+  attr_accessor :should_destroy, :correct
   
   def should_destroy?
     should_destroy.to_i == 1
