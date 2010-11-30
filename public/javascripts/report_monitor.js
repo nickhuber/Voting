2878759db479ad0841e2c7055d9084a11227b578
Kelvin Lui <kelvinlui80@hotmail.com>
2010-11-29 22:29:31 -0800

    Ajax kinda screwy

--- public/javascripts/report_monitor.js
+++ public/javascripts/report_monitor.js
@@ -1,15 +1,4 @@
-
-$(function () {
-    
-    var num_of_participants = $(".num_of_participants").html();
-    var num_of_questions    = $("tbody tr").size();
-    var counter = .5;
-    var data = new Array();
-    var bar_counter;
-    var index = 0;
-    var answer;
-    var newData = [{data: [[0,.5], [1,1]]}];
-    var options = { 
+var options = { 
         bars:{show:true },
         xaxis: {
 	        max : num_of_questions,
@@ -25,33 +14,53 @@ $(function () {
 		    show: false
 		}
     }
+var num_of_questions;
+var num_of_participants;
+$(function () {
+    setInterval(fetchData, 10000);
+    var num_of_participants = $(".num_of_participants").html();
+    var num_of_questions    = $("tbody tr").size();
+    var counter = .5;
+    var data = new Array();
+    var bar_counter;
+    var index = 0;
+    var answer;
+   // var newData = [{data: [[0,.5], [1,1]]}];
     
-
     $("tbody tr").each(function(index,row){
         data[index] = {data: [[index,$('.correct_answer', row).html()]]};
     });
+
+	$.plot($("#placeholder"), data,options);
+      
+});
+function fetchData()
+{
+  //$.ajaxError(error);
     
-    console.log(data);
-     //$.ajax({
+    $.ajax({
                 // usually, we'll just call the same URL, a script
                 // connected to a database, but in this case we only
                 // have static example files so we need to modify the
                 // URL
-             /*   url: "data-eu-gdp-growth-" + iteration + ".json",
+                url: "5",
                 method: 'GET',
                 dataType: 'json',
-                success: onDataReceived*/
-       //     });
-
-    function onDataReceived(series) 
+               
+                success: onDataReceived,
+                error: test 
+     });
+    function test(XMLHttpRequest, textStatus, errorThrown) 
+    {
+        var x = errorThrown;
+      //  alert(XMLHttpRequest);
+    }
+    function onDataReceived(data, textStatus, XMLHttpRequest)
     {
                 // we get all the data in one go, if we only got partial
                 // data, we could merge it with what we already got
-                data = [ series ];
-                
+                data = [ data ];
+                console.log(data);
                 $.plot($("#placeholder"), data, options);
     }
-    
-	$.plot($("#placeholder"), data,options);
-    
-});
+};
