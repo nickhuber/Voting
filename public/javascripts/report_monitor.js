a101844e5062158645059edf29be9f2057ad6c66
Tom Nightingale <tom@tnightingale.com>
2010-12-01 02:13:25 -0800

    fix report question ordering

--- public/javascripts/report_monitor.js
+++ public/javascripts/report_monitor.js
@@ -1,23 +1,24 @@
 var options = { 
-        bars:{show:true },
-        xaxis: {
-	        max : num_of_questions,
-            tickSize: 1,
-            tickAlign: "left"
-		},
-        yaxis: {
-            min : 0,
-            max : num_of_participants,
-            tickSize: 1
-            
-        },
-		legend: {
-		    show: false
-		},
-        hooks: {
-            processOptions: processOptions        
-        }
-    }
+  bars: {
+    show:true 
+  },
+  xaxis: {
+    max : num_of_questions,
+    tickSize: 1,
+    tickAlign: "left"
+  },
+    yaxis: {
+      min : 0,
+      max : num_of_participants,
+      tickSize: 1            
+    },
+  legend: {
+    show: false
+  },
+  hooks: {
+    processOptions: processOptions        
+  }
+}
 
 var graph;
 var num_of_questions;
@@ -25,48 +26,37 @@ var num_of_participants;
 
 function processOptions(plot, options) {
     var labels = new Array();
-    console.log(options);    
-    $(".question_name").each(function(i, item) {
-        labels[i] = new Array();
-        labels[i].push(i);
-        labels[i].push($(item).text());
-    });
+  console.log(options);    
+  $(".question_name").each(function(i, item) {
+      labels[i] = new Array();
+      labels[i].push(i);
+      labels[i].push($(item).text());
+  });
 
-    options.xaxis.ticks = labels;
+  options.xaxis.ticks = labels;
 }
 
 $(document).ready(function(){
-    $("#temp-table").hide();
-    $("#placeholder").show();
-    fetchData();
-    setInterval(fetchData, 4000);
-
     var num_of_participants = $(".num_of_participants").html();
     var num_of_questions    = $("tbody tr").size();
-    var counter = .5;
     var data = new Array();
-    var bar_counter;
     var index = 0;
-    var answer;
-   // var newData = [{data: [[0,.5], [1,1]]}];
+
+    $("#temp-table").hide();
+    $("#placeholder").show();
     
     $(".correct_answer").each(function(index,row){
-        data[index] = {data: [[index,$(row).html()]]};
+        data[index] = {data: [[index, $(row).html()]]};
     });
+    
+    graph = new $.plot($("#placeholder"), data, options);
 
-	graph = new $.plot($("#placeholder"), data, options, 0);
+    //fetchData();
+    setInterval(fetchData, 4000);
     console.log(graph);
-   $('.tickLabel').each(function(idx, el){          
-        //$(el).css('left', 100);
-        //console.log($(el));            
-        //var c = $(el);
-        //var value = c.parent().css('left');
-        // c.parent().css('right',parseInt(value)+1000+'px');
-    });
-      
 });
-function fetchData()
-{   
+
+function fetchData() {   
     $.ajax({
         // usually, we'll just call the same URL, a script
         // connected to a database, but in this case we only
@@ -80,7 +70,6 @@ function fetchData()
      });
    
     function onDataReceived(data, textStatus, XMLHttpRequest) {
-        console.log(data);
         var result = new Array();
         result = data.shift();
 
@@ -90,10 +79,24 @@ function fetchData()
         }
 
         updateTables(result);
-        
-        graph.setData(result.correct_questions);
-        graph.draw();
+        updateGraph(result.correct_questions);
     }
+    
+    function updateGraph(correct_questions) {
+      var data = new Array();
+      for (var i = 0; i < correct_questions.length; i++) {
+        data[i] = {data: new Array()};
+        data[i].data[0] = new Array();
+        data[i].data[0][0] = i;
+        data[i].data[0][1] = correct_questions[i];
+      }
+      
+      console.log(data);
+      $.plot($("#placeholder"), data, options, 0)
+      graph.setData(data);
+      graph.draw();
+    }
+    
     function updateTables(result)
     {
         var index = 0;
